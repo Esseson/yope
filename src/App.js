cd82e05b2948e15c5bdfbc99afeec099c77b6ac4
Esseson <Essesonahmed@yahoo.com>
2019-05-03 09:07:50 -0700

    kelly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component } from 'react';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
 import './App.css';
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const particlesOptions = {
+particles: {
+  number: {
+    value: 50,
+    density: {
+      enable: true,
+      value_area: 1000
+    }
+  }
 }
+}
+
+class App extends Component{
+      render(){
+          return (
+              <div className="App">
+                 <Particles className= "particles"
+                    params={particlesOptions} 
+                  />
+                  <Navigation />
+                  <Logo />
+              </div>
+      
+
+          );
+      };
+
+    }
+    
+      
+
 
 export default App;
